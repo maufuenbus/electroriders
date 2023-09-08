6423ade5e2b29c6efabbe5179725efed9e189c0d
maufuenbus <mau.fuentesb@duocuc.cl>
2023-09-08 16:23:17 -0300

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  motoCross from '../assest/img/motocross.jpg'
+
+function Hero() {
+  return (
+    <>
+    
+    <img className='w-full h-screen object-cover' src={motoCross} />
+    </>
+  )
+}
+
+export default Hero
