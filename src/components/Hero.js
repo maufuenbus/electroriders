0aebe025189e2746919e80a8ff524d62550e1923
maufuenbus <mau.fuentesb@duocuc.cl>
2023-09-08 17:25:05 -0300

    btn ingresa en portada + eliminacion de basura

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,13 @@ import  motoCross from '../assest/img/motocross.jpg'
 function Hero() {
   return (
     <>
-    
-    <img className='w-full h-screen object-cover' src={motoCross} />
+    <div className="relative">
+      <img className='w-full h-screen object-cover' src={motoCross} alt="Imagen de MotoCross" />
+      <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center">
+          <button className="btn-outline btn-warning btn btn-lg sm:btn-sm md:btn-md lg:btn-lg w-1/6 mt-72">Ingresar</button>
+      </div>
+    </div>
+
     </>
   )
 }
