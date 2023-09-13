80c5b8ff5dafa461a35219249190207978c5a4c0
maufuenbus <mau.fuentesb@duocuc.cl>
2023-09-13 18:38:28 -0300

    responsivo ok

--- src/components/pages/Hero.js
+++ src/components/pages/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import motoCross from "../../assets/img/motocross.jpg";
-import logo from "../../assets/img/logo.png";
+import logo from "../../assets/img/logo1.png";
+import '../css/stylesHero.css'
 import { useNavigate } from "react-router-dom";
 
 
@@ -14,25 +15,27 @@ function Hero() {
   return (
     <>
       <div className="relative">
-        <img
-          className="absolute top-40 left-40 w-90 h-80"
-          src={logo}
-          alt="Logo"
-        />
-        <img
-          className="w-full h-screen object-cover"
-          src={motoCross}
-          alt="Imagen de MotoCross"
-        />
-        <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center">
-          <button
-            onClick={handleNavigation}
-            className="btn-outline bg-yellow-300 text-black btn btn-lg sm:btn-sm md:btn-md lg:btn-lg w-1/6 mt-72 glass"
-          >
-            Ingresar
-          </button>
-        </div>
-      </div>
+  <div className="contenedor-logo">
+    <img
+      className="logo"
+      src={logo}
+      alt="Logo"
+    />
+  </div>
+  <img
+    className="background-image "
+    src={motoCross}
+    alt="Imagen de MotoCross"
+  />
+  <div className="absolute top-0 left-0 w-full h-screen flex items-center justify-center">
+    <button
+      onClick={handleNavigation}
+      className="btn btn-warning glass bg-yellow-300 text-black w-1/10 mt-72"
+    >
+      Ingresar
+    </button>
+  </div>
+</div>
     </>
   );
 }
