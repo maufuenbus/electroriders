f7a257198525910f3da79c4cf2f0231abf6e5a69
Yu Lagos Alfaro <betanae@gmail.com>
2023-09-13 21:38:02 -0300

    Edicion footer, agrega paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}',],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'negro': '#000100',
+        'amarillo': '#FFFF00',
+        'verde': '#44AA00',
+      },
+    },
   },
   variants: {},
   plugins: [require('daisyui')],
