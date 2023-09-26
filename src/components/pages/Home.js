7e6d0257316e658a0065e495166ac28cb4304f39
Yu Lagos Alfaro <betanae@gmail.com>
2023-09-26 16:09:23 -0300

    Agrega logos

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../base/Header';
 import Footer from '../base/Footer';
 import motoCross from "../../assets/img/motocross.jpg";
+import logoYellow from "../../assets/img/logoER-YW.png";
+
 
 function Home() {
   return (
@@ -13,11 +15,15 @@ function Home() {
     <div className="hero min-h-screen" style= {{backgroundImage: `url(${motoCross})`}}>
       <div className="hero-overlay bg-opacity-40"></div>
       <div className="hero-content text-center text-white"> 
+
         <div className="max-w-md">
+
+          <img src="{logoYellow}" alt="Logo" />
           <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
           <p className="mb-5">Únete al cambio, convierte con nosotros un nuevo futuro</p>
           <button className="btn btn-primary">Cotizar</button>
         </div>
+      
       </div>
     </div>
 
