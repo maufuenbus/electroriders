cf7fcf4d1ed84a4de015f6f505455d0c8dafc61d
Yu Lagos Alfaro <betanae@gmail.com>
2023-09-26 14:21:36 -0300

    Agrega Hero

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../base/Header';
 import Footer from '../base/Footer';
+import motoCross from "../../assets/img/motocross.jpg";
 
 function Home() {
   return (
@@ -8,9 +9,20 @@ function Home() {
     <div>
       <Header />
     </div>
-    <div className="navbar bg-base-100">
-      <a className="btn btn-ghost normal-case text-xl">home</a>
+
+    <div className="hero min-h-screen" style= {{backgroundImage: `url(${motoCross})`}}>
+      <div className="hero-overlay bg-opacity-40"></div>
+      <div className="hero-content text-center text-white"> 
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">Únete al cambio, convierte con nosotros un nuevo futuro</p>
+          <button className="btn btn-primary">Cotizar</button>
+        </div>
+      </div>
     </div>
+
+    
+    
     <div>
       <Footer />
     </div>    
