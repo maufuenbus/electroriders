31a8e13dff9b5b51db9c01e6f28ec773a582b2ad
maufuenbus <mau.fuentesb@duocuc.cl>
2023-09-26 15:45:09 -0300

    cambio de forma en el form de contacto

--- src/components/pages/Contacto.js
+++ src/components/pages/Contacto.js
@@ -28,23 +28,30 @@ function Contacto() {
   return (
     <>
       <Header />
-      <div className="p-10 bg-black-100 flex justify-center items-center">
-        <form className="bg-white p-8 rounded-lg shadow-lg" onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-sm mb-2" htmlFor="name">Nombre:</label>
-            <input type="text" name="name" id="name" placeholder="Escribe tu nombre" className="w-full border rounded-md p-2" value={formData.name} onChange={handleChange}/>
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm mb-2" htmlFor="email">Correo electrónico: </label>
-            <input type="email" name="email" id="email" placeholder="Escribe tu correo" className="w-full border rounded-md p-2" value={formData.email} onChange={handleChange} />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm mb-2" htmlFor="message"> Mensaje: </label>
-            <textarea name="message" id="message" rows="4" placeholder="Escribe tu mensaje" className="w-full border rounded-md p-2" value={formData.message} onChange={handleChange} ></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary w-full"> Enviar </button>
-        </form>
+      <div className="p-10 bg-black-100 flex">
+        <div className="flex-1">
+          {/*EN ESTE DIV VA EL CONTENIDO DE LA PAGINA*/}
+        </div>
+        
+        <div className="flex-none w-1/3">
+          <form className="bg-white p-8 rounded-lg shadow-lg" onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="name">Nombre:</label>
+              <input type="text" name="name" id="name" placeholder="Escribe tu nombre" className="w-full border rounded-md p-2" value={formData.name} onChange={handleChange}/>
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="email">Correo electrónico: </label>
+              <input type="email" name="email" id="email" placeholder="Escribe tu correo" className="w-full border rounded-md p-2" value={formData.email} onChange={handleChange} />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="message"> Mensaje: </label>
+              <textarea name="message" id="message" rows="4" placeholder="Escribe tu mensaje" className="w-full border rounded-md p-2" value={formData.message} onChange={handleChange} ></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary w-full"> Enviar </button>
+          </form>
+        </div>
       </div>
+
       <Footer />
     </>
   );
