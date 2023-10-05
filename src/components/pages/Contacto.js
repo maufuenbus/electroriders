5dc14c1f60aa9d2e972b617b573d060be9b8069f
Yu Lagos Alfaro <betanae@gmail.com>
2023-10-05 20:43:31 -0300

    Edición contacto

--- src/components/pages/Contacto.js
+++ src/components/pages/Contacto.js
@@ -28,24 +28,25 @@ function Contacto() {
   return (
     <>
       <Header />
-      <div className="p-10 bg-black-100 max-w-7xl mx-auto flex items-center ">
-        <div className="flex-1">
-          {/*EN ESTE DIV VA EL CONTENIDO DE LA PAGINA*/}
+      <div className="p-10 bg-black-100 max-w-4xl mx-auto sm:flex items-center ">
+        <div className="sm:flex-1">
+          <h1 className='animate-slide-left text-left px-6 text-neutral-100 font-bold text-4xl pb-8 pt-22 sm:text-center'><span className='font-bold text-5xl'>¿Te interesa</span> saber más información? <span className='text-amarillo'>¡Contáctanos!</span></h1>
+          
         </div>
         
-        <div className="flex-none w-1/3">
+        <div className="flex-none sm:w-1/2">
           <form className="bg-amarillo p-8 rounded-lg shadow-lg" onSubmit={handleSubmit}>
             <div className="mb-4">
-              <label className="block text-sm mb-2" htmlFor="name">Nombre:</label>
-              <input type="text" name="name" id="name" placeholder="Escribe tu nombre" className="bg-neutral-900 w-full border rounded-md p-2" value={formData.name} onChange={handleChange}/>
+              <label className="block text-sm text-neutral-900 mb-2" htmlFor="name">Nombre:</label>
+              <input type="text" name="name" id="name" placeholder="Escribe tu nombre" className="bg-neutral-900 w-full  rounded-md p-2" value={formData.name} onChange={handleChange}/>
             </div>
             <div className="mb-4">
-              <label className="block text-sm mb-2" htmlFor="email">Correo electrónico: </label>
-              <input type="email" name="email" id="email" placeholder="Escribe tu correo" className="bg-neutral-900 w-full border rounded-md p-2" value={formData.email} onChange={handleChange} />
+              <label className="block text-sm mb-2 text-neutral-900" htmlFor="email">Correo electrónico: </label>
+              <input type="email" name="email" id="email" placeholder="Escribe tu correo" className="bg-neutral-900 w-full  rounded-md p-2" value={formData.email} onChange={handleChange} />
             </div>
             <div className="mb-4">
-              <label className="block text-sm mb-2" htmlFor="message"> Mensaje: </label>
-              <textarea name="message" id="message" rows="4" placeholder="Escribe tu mensaje" className="bg-neutral-900 w-full border rounded-md p-2" value={formData.message} onChange={handleChange} ></textarea>
+              <label className="block text-sm mb-2 text-neutral-900" htmlFor="message"> Mensaje: </label>
+              <textarea name="message" id="message" rows="4" placeholder="Escribe tu mensaje" className="bg-neutral-900 w-full rounded-md p-2" value={formData.message} onChange={handleChange} ></textarea>
             </div>
             <button type="submit" className="btn hover:bg-neutral-50 w-full"> Enviar </button>
           </form>
