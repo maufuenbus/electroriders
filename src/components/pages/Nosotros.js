48e62dc658cec243e8db8527b280fdae423a0614
maufuenbus <mau.fuentesb@duocuc.cl>
2023-10-06 23:56:47 -0300

    se agrega imagen en construcción

--- src/components/pages/Nosotros.js
+++ src/components/pages/Nosotros.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../base/Header';
 import Footer from '../base/Footer';
-
+import EnConstruccionImg from '../../assets/img/En-construccion.jpg';
 function Nosotros() {
   return (
     <>
@@ -9,7 +9,10 @@ function Nosotros() {
       <Header />
     </div>
     <div className="navbar bg-base-100">
-      <a className="btn btn-ghost normal-case text-xl">Nosotros</a>
+        <div className="flex justify-center"> {/* Centrar contenido horizontalmente */}
+            <img src={EnConstruccionImg} alt="En construcción" className="max-w-lg" /> 
+           
+        </div>
     </div>
     <div>
       <Footer />
