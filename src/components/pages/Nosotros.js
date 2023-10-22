f14eec99900e3fb42330d6480f633cc55f0ad2e2
Yu Lagos Alfaro <betanae@gmail.com>
2023-10-22 03:15:40 -0300

    ajustes divisores

--- src/components/pages/Nosotros.js
+++ src/components/pages/Nosotros.js
@@ -3,6 +3,8 @@ import Header from '../base/Header';
 import Footer from '../base/Footer';
 import ariel from "../../assets/img/ariel.jpg";
 import adrenalina from "../../assets/img/adrenalina.jpg";
+import '../css/stylesNosotros.css';
+
 
 //AOS (Animate On Scroll)
 import AOS from "aos";
@@ -41,94 +43,123 @@ function Nosotros() {
             </div>
           </div>
         </div>
+        
       </div>
 
       {/* MISION */}
-      <div className="p-10 bg-black-100 max-w-4xl mx-auto md:flex items-center gap-8 ">
 
-        <div className="md:flex-1/3 mb-8  ">
-        <h1 className='text-center text-neutral-100 font-bold text-5xl pt-10' data-aos="fade-left"><span className='font-bold text-4xl'>Nuestra</span><span className='text-amarillo'> Misión</span></h1>
-        </div>
+      <div className='relative'>
         
-        <div className="flex-none md:w-2/3">
-          <div className="border-amarillo border p-8 rounded-lg shadow-lg" data-aos="fade-up">
-            <div>
-              {/* <p className='text-center max-w-lg mx-auto pb-8 px-6 text-neutral-200 text-xl' data-aos="fade-up">¡Hola! Mi nombre es Ariel Echeverría, y soy el fundador y CEO de ElectroRiders.</p> */}
-              <p className='text-center max-w-lg mx-auto px-4 text-neutral-200 text-lg' data-aos="fade-up">En ElectroRiders, nuestra misión es revolucionar el mundo del deporte extremo, llevando la Electromovilidad a nuevos límites, combinando adrenalina y sostenibilidad en cada aventura.</p>
+        <div className="p-10 bg-black-100 max-w-4xl mx-auto md:flex items-center gap-8 ">
+
+          <div className="md:flex-1/3 mb-8  ">
+          <h1 className='text-center text-neutral-100 font-bold text-5xl pt-10' data-aos="fade-left"><span className='font-bold text-4xl'>Nuestra</span><span className='text-amarillo'> Misión</span></h1>
+          </div>
+
+          <div className="flex-none md:w-2/3">
+            <div className="border-amarillo border p-8 rounded-lg shadow-lg" data-aos="fade-up">
+              <div>
+                {/* <p className='text-center max-w-lg mx-auto pb-8 px-6 text-neutral-200 text-xl' data-aos="fade-up">¡Hola! Mi nombre es Ariel Echeverría, y soy el fundador y CEO de ElectroRiders.</p> */}
+                <p className='text-center max-w-lg mx-auto px-4 text-neutral-100 text-lg' data-aos="fade-up">En ElectroRiders, nuestra misión es revolucionar el mundo del deporte extremo, llevando la Electromovilidad a nuevos límites, combinando adrenalina y sostenibilidad en cada aventura.</p>
+              </div>
+            </div>
+          </div>
+          </div>
+
+          {/* VISION */}
+          <div className="p-10 bg-black-100 max-w-4xl mx-auto md:flex items-center gap-8 ">
+
+          <div className="md:flex-1/3 mb-8  ">
+          <h1 className='text-center text-neutral-100 font-bold text-5xl pt-10' data-aos="fade-left"><span className='font-bold text-4xl'>Nuestra</span><span className='text-amarillo'> Visión</span></h1>
+          </div>
+
+          <div className="flex-none md:w-2/3">
+            <div className="border-amarillo border p-8 rounded-lg shadow-lg" data-aos="fade-up">
+              <div>
+                {/* <p className='text-center max-w-lg mx-auto pb-8 px-6 text-neutral-200 text-xl' data-aos="fade-up">¡Hola! Mi nombre es Ariel Echeverría, y soy el fundador y CEO de ElectroRiders.</p> */}
+                <p className='text-center max-w-lg mx-auto px-0 text-neutral-100 text-lg' data-aos="fade-up">Queremos ser la referencia mundial en Electromovilidad extrema, inspirando pasión y sostenibilidad en cada competición y salto, allanando el camino hacia un futuro más limpio y emocionante.</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* VISION */}
-      <div className="p-10 bg-black-100 max-w-4xl mx-auto md:flex items-center gap-8 ">
+        <div class="custom-shape-divider-bottom-1697953037">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" stroke="none" class="shape-fill"></path>
+            </svg>
+        </div>
 
-        <div className="md:flex-1/3 mb-8  ">
-        <h1 className='text-center text-neutral-100 font-bold text-5xl pt-10' data-aos="fade-left"><span className='font-bold text-4xl'>Nuestra</span><span className='text-amarillo'> Visión</span></h1>
-        </div>
-        
-        <div className="flex-none md:w-2/3">
-          <div className="border-amarillo border p-8 rounded-lg shadow-lg" data-aos="fade-up">
-            <div>
-              {/* <p className='text-center max-w-lg mx-auto pb-8 px-6 text-neutral-200 text-xl' data-aos="fade-up">¡Hola! Mi nombre es Ariel Echeverría, y soy el fundador y CEO de ElectroRiders.</p> */}
-              <p className='text-center max-w-lg mx-auto px-0 text-neutral-200 text-lg' data-aos="fade-up">Queremos ser la referencia mundial en Electromovilidad extrema, inspirando pasión y sostenibilidad en cada competición y salto, allanando el camino hacia un futuro más limpio y emocionante.</p>
-            </div>
-          </div>
-        </div>
       </div>
+      
 
       {/* VALORES */}
 
-      <h1 className='text-center text-neutral-100 font-bold text-6xl pt-10' data-aos="fade-left"><span className='font-bold text-4xl'>Nuestros</span><span className='text-amarillo'> Valores</span></h1>
+      {/* <div className=' bg-amarillo relative h-10 border-amarillo border-2'></div> */}
 
-      <div className="px-10 pb-12 bg-black-100 max-w-6xl mx-auto lg:flex items-center  gap-4">
+      <div className='relative'>
 
-        <div className='sm:flex gap-4 md:max-w-3xl md:mx-auto '>
-          <div className="sm:w-1/2 mb-8  ">
-            <h1 className='text-right lg:text-center text-neutral-100 font-bold text-3xl pt-10' data-aos="fade-left">Innovación</h1>
-            <div className="border-amarillo border rounded-lg shadow-lg md:h-80 lg:h-72" data-aos="fade-up">
-              <div>
-                <img src={adrenalina} alt="Album" className="rounded-lg w-full md:w-200" data-aos="zoom-in" />
-                <p className='text-center max-w-lg mx-auto py-6 text-neutral-200 text-lg sm:px-8 lg:px-8' data-aos="fade-up">Impulsa nuestra creatividad</p>
+        {/* <div class="custom-shape-divider-top-1697950846">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z" stroke="none" class="shape-fill"></path>
+            </svg>
+        </div> */}
+
+        <h1 className='text-center text-neutral-100 font-bold text-6xl pt-10' data-aos="fade-left"><span className='font-bold text-4xl'>Nuestros</span><span className='text-amarillo'> Valores</span></h1>
+
+        <div className="px-10 pb-12 bg-black-100 max-w-6xl mx-auto lg:flex items-center  gap-4">
+
+          <div className='sm:flex gap-4 md:max-w-3xl md:mx-auto '>
+            <div className="sm:w-1/2 mb-8  ">
+              <h1 className='text-right lg:text-center text-neutral-100 font-bold text-3xl pt-10' data-aos="fade-left">Innovación</h1>
+              <div className="border-amarillo border rounded-lg shadow-lg md:h-80 lg:h-72" data-aos="fade-up">
+                <div>
+                  <img src={adrenalina} alt="Album" className="rounded-lg w-full md:w-200" data-aos="zoom-in" />
+                  <p className='text-center max-w-lg mx-auto py-6 text-neutral-200 text-lg sm:px-8 lg:px-8' data-aos="fade-up">Impulsa nuestra creatividad</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="sm:w-1/2 mb-8  ">
+              <h1 className='text-right lg:text-center text-neutral-100 font-bold text-3xl pt-10' data-aos="fade-left">Adrenalina</h1>
+              <div className="border-amarillo border rounded-lg shadow-lg md:h-80 lg:h-72" data-aos="fade-up">
+                <div>
+                  <img src={adrenalina} alt="Album" className="rounded-lg w-full md:w-200" data-aos="zoom-in" />
+                  <p className='text-center max-w-lg mx-auto py-6 text-neutral-200 text-lg sm:px-8 lg:px-8 ' data-aos="fade-up">Nos desafía a superar obstáculos</p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="sm:w-1/2 mb-8  ">
-            <h1 className='text-right lg:text-center text-neutral-100 font-bold text-3xl pt-10' data-aos="fade-left">Adrenalina</h1>
-            <div className="border-amarillo border rounded-lg shadow-lg md:h-80 lg:h-72" data-aos="fade-up">
-              <div>
-                <img src={adrenalina} alt="Album" className="rounded-lg w-full md:w-200" data-aos="zoom-in" />
-                <p className='text-center max-w-lg mx-auto py-6 text-neutral-200 text-lg sm:px-8 lg:px-8 ' data-aos="fade-up">Nos desafía a superar obstáculos</p>
+          <div className='sm:flex gap-4 md:max-w-3xl md:mx-auto'>
+            <div className="sm:w-1/2 mb-8  ">
+              <h1 className='text-right lg:text-center text-neutral-100 font-bold text-3xl pt-10' data-aos="fade-left">Sostenibilidad</h1>
+              <div className="border-amarillo border rounded-lg shadow-lg md:h-80 lg:h-72" data-aos="fade-up">
+                <div>
+                  <img src={adrenalina} alt="Album" className="rounded-lg w-full md:w-200" data-aos="zoom-in" />
+                  <p className='text-center max-w-lg mx-auto py-6 px-2 text-neutral-200 text-lg sm:px-12  lg:px-8' data-aos="fade-up">Guía nuestro compromiso ambiental</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="sm:w-1/2 mb-8 ">
+              <h1 className='text-right lg:text-center  text-neutral-100 font-bold text-3xl pt-10' data-aos="fade-left">Personalización</h1>
+              <div className="border-amarillo border rounded-lg shadow-lg md:h-80 lg:h-72" data-aos="fade-up">
+                <div>
+                  <img src={adrenalina} alt="Album" className="rounded-lg w-full md:w-200" data-aos="zoom-in" />
+                  <p className='text-center max-w-lg mx-auto py-6 px-1 text-neutral-200 text-lg' data-aos="fade-up">Nos adaptamos a las necesidades de nuestros clientes</p>
+                </div>
               </div>
             </div>
           </div>
+
+
         </div>
 
-        <div className='sm:flex gap-4 md:max-w-3xl md:mx-auto'>
-          <div className="sm:w-1/2 mb-8  ">
-            <h1 className='text-right lg:text-center text-neutral-100 font-bold text-3xl pt-10' data-aos="fade-left">Sostenibilidad</h1>
-            <div className="border-amarillo border rounded-lg shadow-lg md:h-80 lg:h-72" data-aos="fade-up">
-              <div>
-                <img src={adrenalina} alt="Album" className="rounded-lg w-full md:w-200" data-aos="zoom-in" />
-                <p className='text-center max-w-lg mx-auto py-6 px-2 text-neutral-200 text-lg sm:px-12  lg:px-8' data-aos="fade-up">Guía nuestro compromiso ambiental</p>
-              </div>
-            </div>
-          </div>
 
-          <div className="sm:w-1/2 mb-8 ">
-            <h1 className='text-right lg:text-center  text-neutral-100 font-bold text-3xl pt-10' data-aos="fade-left">Personalización</h1>
-            <div className="border-amarillo border rounded-lg shadow-lg md:h-80 lg:h-72" data-aos="fade-up">
-              <div>
-                <img src={adrenalina} alt="Album" className="rounded-lg w-full md:w-200" data-aos="zoom-in" />
-                <p className='text-center max-w-lg mx-auto py-6 px-1 text-neutral-200 text-lg' data-aos="fade-up">Nos adaptamos a las necesidades de nuestros clientes</p>
-              </div>
-            </div>
-          </div>
-        </div>
 
-  
       </div>
+
+      
 
       <Footer />
     </>
