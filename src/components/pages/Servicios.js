2af7d2758f5ed74c373b0cbcad33047d572382a2
Yu Lagos Alfaro <betanae@gmail.com>
2023-10-22 23:57:54 -0300

    agrega imagenes Servicios

--- src/components/pages/Servicios.js
+++ src/components/pages/Servicios.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Header from '../base/Header';
 import Footer from '../base/Footer';
-import EnConstruccionImg from '../../assets/img/En-construccion.jpg';
+import asesoria from "../../assets/img/asesoria.jpg";
+import venta from "../../assets/img/venta.jpg";
+import mantenimiento2 from "../../assets/img/mantenimiento2.jpg";
+import conversion2 from "../../assets/img/conversion2.jpg";
+
+
+
+
 function Servicios() {
 
   const servicesList = [
-    { title: 'Transformación de Motores', description: 'Conversión de motos a combustión a eléctricas y kits de conversión.' },
-    { title: 'Asesoría y Consultoría', description: 'Asesoramiento personalizado y capacitación sobre transformaciones eléctricas.' },
-    { title: 'Mantenimiento y Reparación', description: 'Mantenimiento especializado para motos eléctricas y venta de accesorios.' },
-    { title: 'Venta de Motos Eléctricas', description: 'Modelos 100% eléctricos y motos ya convertidas.' },
+    { title: 'Transformación de Motores', description: 'Conversión de motos a combustión a eléctricas y kits de conversión.', imagen: conversion2  },
+    { title: 'Asesoría y Consultoría', description: 'Asesoramiento personalizado y capacitación sobre transformaciones eléctricas.', imagen: asesoria },
+    { title: 'Mantenimiento y Reparación', description: 'Mantenimiento especializado para motos eléctricas y venta de accesorios.', imagen: mantenimiento2 },
+    { title: 'Venta de Motos Eléctricas', description: 'Modelos 100% eléctricos y motos ya convertidas.', imagen: venta },
 
   ];
   return (
@@ -16,13 +23,17 @@ function Servicios() {
     <div>
       <Header />
     </div>
-    <div className="bg-fondo p-10 text-white">
-      <h2 className="text-3xl font-semibold mb-6 text-center text-amarillo">Nuestros Servicios</h2>
+    <div className="bg-fondo p-10 text-white max-w-4xl mx-auto">
+      <h1 className='text-center t-100 text-neutral-100 font-bold text-4xl pb-8' data-aos='fade-right'><span className='font-bold text-3xl'>Nuestros</span><span className='text-amarillo'> Servicios</span></h1>
+
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {servicesList.map((service, index) => (
-          <div key={index} className="p-6 bg-gray-800 rounded shadow-lg">
-            <h3 className="text-xl font-bold mb-4 text-green-500">{service.title}</h3>
-            <p>{service.description}</p>
+          <div key={index} className="p-6 bg-neutral-800 rounded-lg shadow-lg border-amarillo border">
+            <h3 className="text-xl font-bold mb-4 text-amarillo text-center">{service.title}</h3>
+            <img src={service.imagen} alt="Album" className="rounded-lg w-72 mx-auto mb-6" data-aos="zoom-in" />
+
+            <p className='text-center'>{service.description}</p>
+
           </div>
         ))}
       </div>
